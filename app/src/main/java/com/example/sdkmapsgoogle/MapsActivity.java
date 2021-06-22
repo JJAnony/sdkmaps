d8fee231c8bbb2ef8c4f74e525f757f8289fea37
JJAnony <jcolivati@gmail.com>
2021-06-21 22:55:02 -0300

    User Address name

--- app/src/main/java/com/example/sdkmapsgoogle/MapsActivity.java
+++ app/src/main/java/com/example/sdkmapsgoogle/MapsActivity.java
@@ -10,10 +10,13 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,6 +30,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.sdkmapsgoogle.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -61,6 +68,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.clear();
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
                 mMap.addMarker(new MarkerOptions().position(userLocation).title("Maker in you"));
+
+                try {
+                    Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 5);
+                    if (addresses != null && !addresses.isEmpty()) {
+                        for (Address address : addresses) {
+                            Log.d("local", address.toString());
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
             }
         };
